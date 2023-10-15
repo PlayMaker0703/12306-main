4808b8c59ee13683343f3b8d1ec2b9bc99f80821
PlayMaker0703 <zyf20010703@gmail.com>
2023-10-15 19:33:35 +0800

    Initial commit

---
+++ 12306-main/services/aggregation-service/src/main/java/org/opengoofy/index12306/biz/aggregationservice/main1.java
@@ -0,0 +1,10 @@
+package org.opengoofy.index12306.biz.aggregationservice;
+
+public class main1 {
+    public static void main(String[] args) {
+        int i=0;
+        i++;
+        System.out.println(++i);
+        System.out.println(i++);
+    }
+}
